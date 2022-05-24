3a4580816ec8d3f52dce3cad191544370aa5f748
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-05-24 10:21:37 -0400

    Removed TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // TODO (1): Change title
       title: 'Favorities Celebrities',
       theme: ThemeData(
         primarySwatch: Colors.blue,
